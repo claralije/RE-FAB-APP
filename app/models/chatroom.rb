dc6ac3e23d799a65e89685822508fe509c275999
clara lijewski <clara.lijewski@gmail.com>
2021-03-09 11:36:35 +0100

    added the chaaattttt

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,6 +1,14 @@
 class Chatroom < ApplicationRecord
-  belongs_to :user_a, class_name: 'User', foreign_key: 'user_a'
-  belongs_to :user_b, class_name: 'User', foreign_key: 'user_b'
+  belongs_to :user_a, class_name: 'User'
+  belongs_to :user_b, class_name: 'User'
 
   has_many :messages
+
+  def other_user(current_user)
+    if self.user_a == current_user
+      return user_b
+    else
+      return user_a
+    end
+  end
 end
