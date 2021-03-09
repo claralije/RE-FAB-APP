01cf4ab1c46e801934d4e4acca8ffd997e76704a
rosemarie sutanto <rosemariesutanto@gmail.com>
2021-03-09 13:59:25 +0700

    removed authenticate user skip for create update and delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @products = Product.all
