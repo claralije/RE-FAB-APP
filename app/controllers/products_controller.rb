ef67f6438186c1a4f1b1e44231c2f65493b7931d
rosemarie sutanto <rosemariesutanto@gmail.com>
2021-03-18 14:41:33 +0700

    wip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  #skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @products = Product.where.not(status: 'sold')
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
 require 'faker'
 # Examples:
-Review.destroy_all
-Product.destroy_all
 Favorite.destroy_all
+Review.destroy_all
+Chatroom.destroy_all
 Deal.destroy_all
+Product.destroy_all
 User.destroy_all
 
 cities = ["los angeles", "berlin", "paris", "pyongyang", "moscow", "canggu", "francheville", "barcelona", "singapore", "jakarta", "london", "miami", "las vegas", "ibiza"]
 schools = ['Central Saint Martins', 'London College Of Fashion', 'Westminster', 'Goldsmiths', 'Istituto Marangoni', 'Kingston', 'Royal College of Art']
 status = ['pending', 'in_process', 'closed']
 colors = ["White", "Yellow", "Blue", "Red", "Green", "Black", "Brown", "Azure", "Ivory", "Teal", "Silver", "Purple", "Navy blue", "Pea green", "Gray", "Orange", "Maroon", "Charcoal", "Aquamarine", "Coral", "Fuchsia", "Wheat", "Lime", "Crimson", "Khaki", "Hot pink", "Magenta", "Olden", "Plum", "Olive", "Cyan"]
+
 
 30.times do
 user = User.create!(
@@ -21,6 +23,7 @@ user = User.create!(
   school: schools.sample,
   average_rating: rand(1...5),
   )
+puts "#{user.name} created"
 end
 
 
@@ -28,45 +31,195 @@ fabric = ['silk', 'cotton', 'polyester', 'linen', 'leather', 'tencel', 'wool', '
 condition = ['new', 'used']
 product_status = ['available', 'sold']
 
-30.times do
-product = Product.create!(
-  title: Faker::Game.title,
-  description: 'The best fabric of all time',
-  fabric: fabric.sample,
-  color: colors.sample,
-  weight: rand(1...10),
-  condition: condition.sample,
-  status: product_status.sample,
-  quantity: rand(1...30),
-  location: cities.sample,
-  user: User.all.sample,
-  price: rand(5...400)
-  )
-end
+products = [
+  {
+    title: "QiBao Silk",
+    description: '100% genuine silk textile, ideal for traditional Chinese garments and especially Chinese New Year.',
+    fabric: 'silk',
+    color: 'Red',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: 'available',
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(20...50)
+  }
+]
 
-30.times do
-deal = Deal.create!(
-  status: status.sample,
-  user: User.all.sample,
-  product: Product.all.sample,
-)
-  review = [true, false].sample
-  if review
-    review = Review.create(
-    title: Faker::Game.title,
-    content: Faker::Restaurant.review,
-    rating: rand(1...5),
-    deal: deal,
-    user: deal.user
-    )
-  end
-end
+products = [
+  {
+    title: "Calico",
+    description: 'I am pivoting away from fashion and need to get rid of all this calico that I bought to toile with. Happy to negotiate on price as I want them all gone.',
+    fabric: 'cotton',
+    color: 'Ivory',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: 3
+  },
+  {
+    title: "Hessian",
+    description: 'Bought this for a potato sack photoshoot, have no use for it anymore however',
+    fabric: 'cotton',
+    color: 'Khaki',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(1...10)
+  },
+  {
+    title: "Baby blue tulle",
+    description: 'Planned to make tutus with it but my schedule is just way too busy. Bought at Brick Lane fabric stores.',
+    fabric: 'polyester',
+    color: 'Blue',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(1...5)
+  },
+  {
+    title: "Gold Sassy Leather",
+    description: 'Impulse purchase at a famous Drag Queen\'s yard sale',
+    fabric: 'leather',
+    color: 'Yellow',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(5...40)
+  },
+  {
+    title: "Graduate CSM BA Knit SAMPLE",
+    description: 'One of the bigger samples I produced during my knit course at CSM, it\'s just collecting dust and I reckon someone could make something good out of it',
+    fabric: 'wool',
+    color: 'Coral',
+    weight: rand(1...10),
+    condition: 'new',
+    status: product_status.sample,
+    quantity: 1,
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(10...40)
+  },
+  {
+    title: "beige faux fur",
+    description: 'Bought this for a perfume shoot I did for a client, but I have no use for it anymore',
+    fabric: 'faux fur',
+    color: 'Ivory',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(5...20)
+  },
+  {
+    title: "Deep blue sequined fabric",
+    description: 'Made some sassy trousers out of this to wear to a festival, bought more thinking i\'d make a whole outfit but I got lazy',
+    fabric: 'polyester',
+    color: 'Blue',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(5...10)
+  },
+  {
+    title: "White cotton good for shirts",
+    description: 'Made a shirt out of it, easy to work with, still got 2m to spare, need to get rid as I am moving out soon',
+    fabric: 'cotton',
+    color: 'White',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(5...400)
+  },
+  {
+    title: "Theatrical Organza",
+    description: 'used by the costume making department at the Royal Theatre',
+    fabric: 'polyester',
+    color: 'Blue',
+    weight: rand(1...10),
+    condition: condition.sample,
+    status: product_status.sample,
+    quantity: rand(1...30),
+    location: cities.sample,
+    user: User.all.sample,
+    price: rand(5...30)
+  }
+]
 
-30.times do
-fav = Favorite.create!(
-  user: User.all.sample,
-  product: Product.all.sample
-  )
+count = 1
+products.each do |x|
+  product = Product.create!(x)
+  puts "Product #{product.title} created"
+  path = Rails.root.join("db/photos/#{count}.jpg")
+  file = File.open(path)
+  product.photos.attach(io: file, filename: "#{product.title}_pic_1.jpg", content_type: ('image/jpg'))
+  puts "Product #{product.title} 1 image attached"
+  path = Rails.root.join("db/photos/#{count + 1}.jpg")
+  file = File.open(path)
+  product.photos.attach(io: file, filename: "#{product.title}_pic_1.jpg", content_type: ('image/jpg'))
+  puts "Product #{product.title} 2 images attached"
+  count += 2
 end
+# 30.times do
+# product = Product.create!(
+#   title: Faker::Game.title,
+#   description: 'The best fabric of all time',
+#   fabric: fabric.sample,
+#   color: colors.sample,
+#   weight: rand(1...10),
+#   condition: condition.sample,
+#   status: product_status.sample,
+#   quantity: rand(1...30),
+#   location: cities.sample,
+#   user: User.all.sample,
+#   price: rand(5...400)
+#   )
+# end
+
+# 30.times do
+# deal = Deal.create!(
+#   status: status.sample,
+#   user: User.all.sample,
+#   product: Product.all.sample,
+# )
+#   review = [true, false].sample
+#   if review
+#     review = Review.create(
+#     title: Faker::Game.title,
+#     content: Faker::Restaurant.review,
+#     rating: rand(1...5),
+#     deal: deal,
+#     user: deal.user
+#     )
+#   end
+# end
+
+# 30.times do
+# fav = Favorite.create!(
+#   user: User.all.sample,
+#   product: Product.all.sample
+#   )
+# end
 
 
